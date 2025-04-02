7f2c92d68021139a84c433c5f95bed5fc470c664
Chen Zaichuang <1158400735@qq.com>
2025-04-02 10:03:58 +0800

    change to manifest_version v3

--- src/background.js
+++ src/background.js
@@ -1,173 +1,144 @@
 'use strict';
 
-async function toJson(response) {
-    return await response.json()
-}
-
 async function getLabelMap(host, sObjectName, sObjectId, headers) {
-    return await fetch(host + '/services/data/v37.0/sobjects/' + sObjectName + '/' + sObjectId, {headers: headers}).then(toJson).then(async data => {
-        let recordTypeId = data.RecordTypeId || '012000000000000AAA';
-        let labelMap = await fetch(host + '/services/data/v37.0/sobjects/' + sObjectName + '/describe/layouts/' + recordTypeId, {headers: headers}).then(toJson).then(data => {
-            data = data.layouts != null ? data.layouts[0] : data;
-            let labelMap = {};
-            let labelShowTimes = {};
-            data.detailLayoutSections.map(section => {
-                section.layoutRows.map(row => {
-                    row.layoutItems.filter(item => !item.placeHolder).map(item => {
-                        labelShowTimes[item.label] = labelShowTimes[item.label] !== undefined ? labelShowTimes[item.label] + 1 : 0;
-                        let m = labelMap[item.label] ? labelMap[item.label] : {};
-                        m[labelShowTimes[item.label]] = item.layoutComponents[0] && item.layoutComponents[0].value;
-                        labelMap[item.label] = m;
-                    })
-                })
+    const response = await fetch(`${host}/services/data/v37.0/sobjects/${sObjectName}/${sObjectId}`, { headers });
+    const data = await response.json();
+
+    const recordTypeId = data.RecordTypeId || '012000000000000AAA';
+    const layoutResponse = await fetch(`${host}/services/data/v37.0/sobjects/${sObjectName}/describe/layouts/${recordTypeId}`, { headers });
+    const layoutData = await layoutResponse.json();
+
+    const labelMap = {};
+    const labelShowTimes = {};
+    (layoutData.layouts?.[0]?.detailLayoutSections || layoutData.detailLayoutSections).forEach(section => {
+        section.layoutRows?.forEach(row => {
+            row.layoutItems?.filter(item => !item.placeHolder).forEach(item => {
+                labelShowTimes[item.label] = (labelShowTimes[item.label] || -1) + 1;
+                labelMap[item.label] = labelMap[item.label] || {};
+                labelMap[item.label][labelShowTimes[item.label]] = item.layoutComponents[0]?.value;
             });
-            return labelMap;
         });
+    });
 
-        let assistLabelMap = await fetch(host + '/services/data/v37.0/sobjects/' + sObjectName + '/describe', {headers: headers}).then(toJson).then(data => {
-            let assistLabelMap = {};
-            data.fields.map(field => {
-                if (labelMap[field.label] === undefined) {
-                    assistLabelMap[field.label] = field.name;
-                }
-            });
+    const describeResponse = await fetch(`${host}/services/data/v37.0/sobjects/${sObjectName}/describe`, { headers });
+    const describeData = await describeResponse.json();
 
-            if (sObjectName === 'User') {
-                assistLabelMap['Debug Mode'] = 'UserPreferencesUserDebugModePref';
-                assistLabelMap['Delegated Approver'] = 'DelegatedApproverId';
-                assistLabelMap['Individual'] = 'IndividualId';
-                assistLabelMap['Failed Login Attempts'] = 'NumberOfFailedLogins';
-                assistLabelMap['Cache Diagnostics'] = 'UserPreferencesCacheDiagnostics';
-                assistLabelMap['Development Mode'] = 'UserPreferencesApexPagesDeveloperMode';
-                assistLabelMap['Modified By'] = 'LastModifiedById';
-            }
+    const assistLabelMap = {};
+    describeData.fields?.forEach(field => {
+        if (!labelMap[field.label]) {
+            assistLabelMap[field.label] = field.name;
+        }
+    });
 
-            return assistLabelMap;
+    if (sObjectName === 'User') {
+        Object.assign(assistLabelMap, {
+            'Debug Mode': 'UserPreferencesUserDebugModePref',
+            'Delegated Approver': 'DelegatedApproverId',
+            'Individual': 'IndividualId',
+            'Failed Login Attempts': 'NumberOfFailedLogins',
+            'Cache Diagnostics': 'UserPreferencesCacheDiagnostics',
+            'Development Mode': 'UserPreferencesApexPagesDeveloperMode',
+            'Modified By': 'LastModifiedById'
         });
+    }
 
-        return {labelMap: labelMap, assistLabelMap: assistLabelMap};
-    })
+    return { labelMap, assistLabelMap };
 }
 
 async function getSObjectName(host, headers, keyPrefix) {
-    return await fetch(host + '/services/data/v37.0/sobjects', {
-        method: 'GET',
-        headers: headers
-    }).then(toJson).then(data => {
-        let sObjectName;
-        for (var i = 0; i < data.sobjects.length; i++) {
-            if (data.sobjects[i].keyPrefix === keyPrefix) {
-                sObjectName = data.sobjects[i].name;
-                break;
-            }
-        }
-        return sObjectName;
-    });
+    const response = await fetch(`${host}/services/data/v37.0/sobjects`, { headers });
+    const data = await response.json();
+    return data.sobjects?.find(obj => obj.keyPrefix === keyPrefix)?.name;
 }
 
 async function getTokenAndDomainInClassic(url) {
-    return new Promise((resolve, reject) => {
-        chrome.cookies.getAll({"url": url, "name": "sid"}, function (cookies) {
-            resolve([cookies[0].value, cookies[0].domain]);
-        });
-    })
+    const cookies = await chrome.cookies.getAll({ url, name: 'sid' });
+    return cookies[0] ? [cookies[0].value, cookies[0].domain] : [null, null];
 }
 
-async function getTokenAndDomainInLightning(url, customDomain) {
-    return new Promise((resolve, reject) => {
-        chrome.cookies.getAll({"domain": "salesforce.com", "name": "sid"}, function (cookies) {
-            resolve(cookies);
-        });
-    }).then(cookies => {
-        let token = "";
-        let domain = "";
-        for(let cookie of cookies){
-            if (cookie.domain.startsWith(customDomain + ".")){
-                token = cookie.value;
-                domain = cookie.domain;
-                break;
-            }
+async function getTokenAndDomainInLightning(customDomain) {
+    const cookies = await chrome.cookies.getAll({ domain: 'salesforce.com', name: 'sid' });
+    for (const cookie of cookies) {
+        if (cookie.domain.startsWith(`${customDomain}.`)) {
+            return [cookie.value, cookie.domain];
         }
-        return [token, domain];
-    })
+    }
+    return [null, null];
 }
 
 function getLongId(originalId) {
-    if (originalId.length < 15) {
-        return "";
-    } else {
-        originalId = originalId.substring(0, 15)
-    }
+    if (!originalId || originalId.length < 15) return '';
+    originalId = originalId.substring(0, 15);
 
-    let addon = "";
-    for (var block = 0; block < 3; block++) {
-        var loop = 0;
-        for (var position = 0; position < 5; position++) {
-            var current = originalId.charAt(block * 5 + position);
-            if (current >= "A" && current <= "Z")
-                loop += 1 << position;
+    let addon = '';
+    for (let block = 0; block < 3; block++) {
+        let loop = 0;
+        for (let position = 0; position < 5; position++) {
+            const current = originalId.charAt(block * 5 + position);
+            if (current >= 'A' && current <= 'Z') loop += 1 << position;
         }
-        addon += "ABCDEFGHIJKLMNOPQRSTUVWXYZ012345".charAt(loop);
+        addon += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ012345'.charAt(loop);
     }
     return originalId + addon;
 }
 
 async function showApiName(tab) {
-    const url = new URL(tab.url)
-    let protocol = url.protocol;
-    let host = url.host;
-    let path = url.pathname;
-    let isLightningMode = host.includes("lightning.force.com");
-    let matchResult;
-    let sid, domain;
-    if (isLightningMode) {
-        matchResult = path.match(/\/lightning\/r\/(\w+)\/(\w+)\W*/);
-        if (!matchResult) {
-            return;
+    try {
+        const url = new URL(tab.url);
+        const isLightning = url.host.includes('lightning.force.com');
+
+        let sid, domain;
+        if (isLightning) {
+            const customDomain = url.host.split('.')[0];
+            [sid, domain] = await getTokenAndDomainInLightning(customDomain);
+        } else {
+            [sid, domain] = await getTokenAndDomainInClassic(url.origin);
         }
-        let customDomain = host.substring(0, host.indexOf(".lightning.force.com"));
-        [sid, domain] = await getTokenAndDomainInLightning(protocol + '//' + host, customDomain);
-    } else {
-        if (!path.match(/\/\w{15,18}/)) {
-            return;
+
+        if (!sid) return;
+
+        const headers = {
+            'Authorization': `Bearer ${sid}`,
+            'Content-Type': 'application/json'
+        };
+
+        const apiHost = `https://${isLightning ? domain : url.host}`;
+        const pathSegments = url.pathname.split('/');
+
+        let sObjectId, sObjectName;
+        if (isLightning) {
+            const match = url.pathname.match(/\/lightning\/r\/(\w+)\/(\w+)/);
+            if (!match) return;
+            sObjectName = match[1];
+            sObjectId = match[2];
+        } else {
+            sObjectId = pathSegments[1]?.substring(0, 15);
+            const keyPrefix = pathSegments[1]?.substring(0, 3);
+            sObjectName = await getSObjectName(apiHost, headers, keyPrefix);
         }
-        [sid, domain] = await getTokenAndDomainInClassic(protocol + '//' + host, "");
+
+        if (!sObjectName || !sObjectId) return;
+
+        const labelMap = await getLabelMap(apiHost, sObjectName, sObjectId, headers);
+        const longId = getLongId(sObjectId);
+
+        await chrome.tabs.sendMessage(tab.id, {
+            command: "showApiName",
+            isLightningMode: isLightning,
+            labelMap,
+            sObjectName,
+            longId
+        });
+    } catch (error) {
+        console.error('Error in showApiName:', error);
     }
-    let sObjectId;
-    let sObjectName;
-
-    let apiHost = protocol + '//' + (isLightningMode ? domain : host)
-    let headers = {"Authorization": `Bearer ${sid}`, "Content-Type": "application/json"};
-    if (isLightningMode) {
-        sObjectId = matchResult[2];
-        sObjectName = matchResult[1];
-    } else {
-        sObjectId = path.substring(1, 16);
-        sObjectName = await getSObjectName(apiHost, headers, path.substring(1, 4));
-    }
-
-    let labelMap = await getLabelMap(apiHost, sObjectName, sObjectId, headers);
-
-    await chrome.tabs.sendMessage(tab.id, {
-        command: "showApiName",
-        isLightningMode: isLightningMode,
-        labelMap: labelMap,
-        sObjectName: sObjectName,
-        longId: getLongId(sObjectId)
-    });
 }
 
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-    showApiName(tab).then(res => console.log(res)).catch(res => console.log(res));
-});
-
-
-chrome.commands.onCommand.addListener(function (command) {
+// Event Listeners
+chrome.action.onClicked.addListener(showApiName);
+chrome.commands.onCommand.addListener((command) => {
     if (command === 'showApiName') {
-        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            showApiName(tabs[0]).then(res => console.log(res)).catch(res => console.log(res));
-        });
+        chrome.tabs.query({ active: true, currentWindow: true })
+            .then(([tab]) => tab && showApiName(tab));
     }
 });
-
--- src/content_script.js
+++ src/content_script.js
@@ -1,79 +1,94 @@
 'use strict';
 
-function appendSpanToElement(el, content, toBelow) {
-    let apiElement = document.createElement('span');
-    apiElement.textContent = content;
-    if (toBelow) {
-        apiElement.className = "apinames-script-class";
-        apiElement.style = 'display: block;font-weight: normal;color: #A9A9A9;margin-top: 3px;margin-bottom: 5px;';
-    } else {
-        apiElement.style = 'display: inline-table; margin-left: 1rem';
-    }
-    el.appendChild(apiElement);
+function appendSpanToElement(el, content, isBelow) {
+    const span = document.createElement('span');
+    span.textContent = content;
+    span.className = 'apinames-script-element';
+    span.style = isBelow
+        ? 'display: block; font-weight: normal; color: #A9A9A9; margin: 3px 0 5px;'
+        : 'display: inline-table; margin-left: 1rem;';
+    el.appendChild(span);
 }
 
-function appendSpansToElement(el, contentArray) {
-    let divElement = document.createElement('div');
-    divElement.style = 'position: absolute; display: inline-block; font-weight: normal;color: #A9A9A9;';
-    divElement.className = "apinames-script-class";
-    for (let content of contentArray) {
-        appendSpanToElement(divElement, content, false);
-    }
+function appendSpansToElement(el, contents) {
+    const container = document.createElement('div');
+    container.className = 'apinames-script-container';
+    container.style = 'position: absolute; display: inline-block; color: #A9A9A9;';
+    contents.forEach(content => appendSpanToElement(container, content, false));
     el.style.position = 'relative';
-    el.appendChild(divElement);
+    el.appendChild(container);
 }
 
-function addObjectAPIName(objectClass, objectAPIName, longId) {
-    let objectLabelElements = document.querySelectorAll(objectClass);
-    if (objectLabelElements.length > 0) {
-        let objectLabelElement = objectLabelElements[objectLabelElements.length - 1];
-        appendSpansToElement(objectLabelElement, [objectAPIName, longId])
+function addObjectAPIName(selector, apiName, longId) {
+    const elements = document.querySelectorAll(selector);
+    if (elements.length > 0) {
+        appendSpansToElement(elements[elements.length - 1], [apiName, longId]);
     }
 }
 
-function addFieldAPIName(fieldClass, filter, getFieldFunc, labelMap) {
-    let fieldElements = document.querySelectorAll(fieldClass);
-    let labelShowTimes = {};
-    Array.prototype.map.call(fieldElements, el => {
+function addFieldAPIName(selector, filter, labelExtractor, labelMap) {
+    const elements = document.querySelectorAll(selector);
+    const labelCounts = {};
+
+    elements.forEach(el => {
         if (filter(el)) {
-            let fieldLabel = getFieldFunc(el);
-            if (labelMap.labelMap[fieldLabel] != null) {
-                labelShowTimes[fieldLabel] = labelShowTimes[fieldLabel] !== undefined ? labelShowTimes[fieldLabel] + 1 : 0;
-                appendSpanToElement(el, labelMap.labelMap[fieldLabel][labelShowTimes[fieldLabel]], true);
-            } else if (labelMap.assistLabelMap[fieldLabel] != null) {
-                appendSpanToElement(el, labelMap.assistLabelMap[fieldLabel], true);
-            } else if (fieldLabel.toLowerCase().includes('currency')) {
-                appendSpanToElement(el, 'CurrencyIsoCode', true);
-            }
+            const label = labelExtractor(el);
+            const apiName = findApiName(label, labelCounts, labelMap);
+            if (apiName) appendSpanToElement(el, apiName, true);
         }
-    })
+    });
 }
 
-async function replaceWithAPINames(isLightningMode, sObjectName, labelMap, longId) {
+function findApiName(label, counts, { labelMap, assistLabelMap }) {
+    counts[label] = (counts[label] || -1) + 1;
 
-    window.showAPINameScript = window.showAPINameScript || {};
-    window.showAPINameScript.isOn = window.showAPINameScript.isOn === undefined ? false : !window.showAPINameScript.isOn;
+    if (labelMap?.[label]?.[counts[label]]) {
+        return labelMap[label][counts[label]];
+    }
+    if (assistLabelMap?.[label]) {
+        return assistLabelMap[label];
+    }
+    if (label.toLowerCase().includes('currency')) {
+        return 'CurrencyIsoCode';
+    }
+    return null;
+}
 
-    if (!window.showAPINameScript.isOn) {
-        if (isLightningMode) {
-            addFieldAPIName('.test-id__field-label-container.slds-form-element__label', el => {return el.childNodes.length > 0}, el => {return el.childNodes[0].innerText}, labelMap);
+async function toggleAPIDisplay(isLightning, sObjectName, labelMap, longId) {
+    window.showAPINameScript = window.showAPINameScript || { isOn: false };
+
+    const elements = document.querySelectorAll('.apinames-script-element, .apinames-script-container');
+    if (window.showAPINameScript.isOn) {
+        elements.forEach(el => el.remove());
+    } else {
+        if (isLightning) {
+            addFieldAPIName(
+                '.test-id__field-label-container.slds-form-element__label',
+                el => el.childNodes.length > 0,
+                el => el.firstChild?.innerText,
+                labelMap
+            );
             addObjectAPIName('.entityNameTitle', sObjectName, longId);
         } else {
-            addFieldAPIName('.labelCol', _ => {return true}, el => {return el.textContent.split('sfdcPage.')[0]}, labelMap);
+            addFieldAPIName(
+                '.labelCol',
+                () => true,
+                el => el.textContent.split('sfdcPage.')[0],
+                labelMap
+            );
             addObjectAPIName('.pageType', sObjectName, longId);
         }
-    } else {
-        let els = document.querySelectorAll('.apinames-script-class');
-        Array.prototype.map.call(els, el => {
-            el.parentNode.removeChild(el);
-        });
     }
+    window.showAPINameScript.isOn = !window.showAPINameScript.isOn;
 }
 
-chrome.runtime.onMessage.addListener(function (message) {
-    switch (message.command) {
-        case "showApiName":
-            replaceWithAPINames(message.isLightningMode, message.sObjectName, message.labelMap, message.longId).then(res => {}).catch(res => console.log(res));
-            break;
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.command === 'showApiName') {
+        toggleAPIDisplay(
+            message.isLightningMode,
+            message.sObjectName,
+            message.labelMap,
+            message.longId
+        );
     }
 });
